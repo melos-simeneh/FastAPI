bc71e21ad32a5e1575ae8c837aa30fd2566a0e92
Melos <85923940+melos-simeneh@users.noreply.github.com>
2023-09-04 12:44:25 -0700

    update user endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI,HTTPException
 from typing import List
-from models import User,Gender,Role
+from models import User,Gender,Role,UserUpdateRequest
 from uuid import UUID,uuid4
 
 app=FastAPI()
@@ -24,6 +24,22 @@ async def fetch_users():
 async def create_user(user:User):
     db.append(user)
     return user
+
+@app.put("/api/users/{id}")
+async def update_user(user_update:UserUpdateRequest,id):
+    for user in db:
+        if user.id==id:
+            if user_update.first_name is not None:
+               user.first_name=user_update.first_name
+            if user_update.last_name is not None:
+               user.last_name=user_update.last_name
+            if user_update.middle_name is not None:
+               user.middle_name=user_update.middle_name
+            if user_update.roles is not None:
+               user.roles=user_update.roles
+            return user
+    
+    raise HTTPException(status_code=404,detail= f"User with id {id} not found")
 
 @app.delete("/api/users/{id}")
 async def delete_user(id:UUID):
